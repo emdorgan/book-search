60e5d28d53bd688a49e61ea45fdeb20c59b58c28
Emily Dorgan <wmdorgan@gmail.com>
2021-11-28 11:53:01 -0800

    set up the client-side mutations for signup

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -29,7 +29,7 @@ const LoginForm = () => {
 
     try {
       const { data } = await login({
-        variables: { ...formState },
+        variables: { ...userFormData },
       });
 
       // if (!response.ok) {
